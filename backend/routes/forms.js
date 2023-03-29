42b004562daf549ac7e39b5d8eff74e6cd502323
deepak <bairwaddeepak@gmail.com>
2023-03-29 13:56:07 +0530

    .

---
+++ backend/routes/forms.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/users");
+router.post("/create", async (req, res) => {
+  try {
+    await User({
+      name: req.body.name,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      hobbies: req.body.hobbies,
+    }).save();
+    res.status(201);
+    res.send({ message: "created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.send({ message: "something went wrong" });
+  }
+});
+router.get("/data", (req, res) => {
+  User.find()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
+router.delete(`/data/items/:id`, async (req, res) => {
+  User.findByIdAndDelete(req.params.id).then(() => {
+    res.send({ message: "deleted successfully" });
+  });
+  User.fin;
+});
+
+module.exports = router;
